7a201c982f45cd77baf5773c2ca390552d47c292
huangyizhi <huangyizhi@hnu.edu.cn>
2020-09-27 19:46:52 +0800

    [who] huangyizhi

--- src/van.cc
+++ src/van.cc
@@ -608,7 +608,7 @@ void Van::Receiving_(Meta& nodes, Meta& recovery_nodes)
     if (!msg.meta.control.empty()) {
       // control msg
       auto& ctrl = msg.meta.control;
-      printf("[%s] % recv msg: %d, ctrl.cmd: %d, from %d\n", __FUNCTION__, msg.meta.recver, ctrl.cmd, msg.meta.sender);
+      printf("[%s] %d recv msg, ctrl.cmd: %d, from %d, size: %d\n", __FUNCTION__, msg.meta.recver, ctrl.cmd, msg.meta.sender, recv_bytes);
       if (ctrl.cmd == Control::TERMINATE) {
         ProcessTerminateCommand();
       } else if (ctrl.cmd == Control::ADD_NODE) {
@@ -624,6 +624,6 @@ void Van::Receiving_(Meta& nodes, Meta& recovery_nodes)
       ProcessDataMsg(&msg);
     }
 
-    printf("Van::Receiving_ process data success!\n");
+    printf("Van::Receiving_ %d process data success!\n", my_node_.id);
 }
 }  // namespace ps
